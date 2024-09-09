83d1bc380c6ebf6ef38880ae7c45533978aa5f01
Carlos Hernandez <carloshernandezp21@gmail.com>
2024-09-09 18:19:34 +0200

    refactor(Estructura de Archivos) Organizar la estructura de los archivos

--- index.js
+++ index.js
@@ -1,129 +1,25 @@
-let products = {
-    data: [{
-        productName: "Cotton Pants",
-        category: "Bottomwear",
-        price: "35",
-        image: "img/beige-cotton-pants.jpg"
-    },
-    {
-        productName: "Beige Jacket",
-        category: "Jacket",
-        price: "40",
-        image: "img/beige-jacket.jpg"
-    },
-    {
-        productName: "Beige Shirt",
-        category: "Topwear",
-        price: "25",
-        image: "img/beige-shirt.jpg"
-    },
-    {
-        productName: "Beige Short",
-        category: "Bottomwear",
-        price: "30",
-        image: "img/beige-short.jpg"
-    },
-    {
-        productName: "Beige Swearter",
-        category: "Topwear",
-        price: "35",
-        image: "img/beige-sweater.jpg"
-    },
-    {
-        productName: "Black Jogger",
-        category: "Bottomwear",
-        price: "30",
-        image: "img/black-jogger.jpg"
-    },
-    {
-        productName: "Black T-shirt",
-        category: "Topwear",
-        price: "20",
-        image: "img/black-t-shirt.jpg"
-    },
-    {
-        productName: "Blue Jean Jacket",
-        category: "Jacket",
-        price: "40",
-        image: "img/blue-jean-jacket.jpg"
-    },
-    {
-        productName: "Blue Shirt",
-        category: "Topwear",
-        price: "20",
-        image: "img/blue-shirt.jpg"
-    },
-    {
-        productName: "Brown Watch",
-        category: "Watch",
-        price: "70",
-        image: "img/brown-watch.jpg"
-    },
-    {
-        productName: "Gray Pants",
-        category: "Bottomwear",
-        price: "40",
-        image: "img/gray-pants.jpg"
-    },
-    {
-        productName: "Green Jacket",
-        category: "Jacket",
-        price: "50",
-        image: "img/green-jacket.jpg"
-    },
-    {
-        productName: "Green Pants",
-        category: "Bottomwear",
-        price: "35",
-        image: "img/green-pants.jpg"
-    },
-    {
-        productName: "Green Shirt",
-        category: "Topwear",
-        price: "20",
-        image: "img/green-shirt.jpg"
-    },
-    {
-        productName: "Light Blue Pants",
-        category: "Bottomwear",
-        price: "55",
-        image: "img/light-blue-pants.jpg"
-    },
-    {
-        productName: "Sleeveless Gray Shirt",
-        category: "Topwear",
-        price: "35",
-        image: "img/sleeveless-gray-shirt.jpg"
-    },
-    {
-        productName: "Sporty Smart Watch",
-        category: "Watch",
-        price: "70",
-        image: "img/sporty-black-watch.jpg"
-    },
-],
-};
+import products from "./src/data/products.js";
 
-for (let i of products.data) {
+for (let i of products) {
     //create the cards for the products
-    let card = document.createElement("div");
+    const card = document.createElement("div");
     //card should have category
     card.classList.add("card", i.category, "hidden");
     //image div
-    let imgContainer = document.createElement("div");
+    const imgContainer = document.createElement("div");
     imgContainer.classList.add("img-container");
     //craete the image 
-    let image = document.createElement("img");
+    const image = document.createElement("img");
     image.setAttribute("src", i.image);
     imgContainer.appendChild(image);
     card.appendChild(imgContainer);
     //create and append the description to the image container
-    let description = document.createElement("h5");
+    const description = document.createElement("h5");
     description.classList.add("description")
     description.innerHTML = (i.productName + "<br>" + "$" + i.price).toUpperCase();
     card.appendChild(description);
     //append the card to the dom
-    let productsContainer = document.querySelector(".products-container");
+    const productsContainer = document.querySelector(".products-container");
     productsContainer.appendChild(card);
 }
 
---
+++ src/data/products.js
@@ -0,0 +1,105 @@
+ const products = [{
+    productName: "Cotton Pants",
+    category: "Bottomwear",
+    price: "35",
+    image: "./src/img/beige-cotton-pants.jpg"
+},
+{
+    productName: "Beige Jacket",
+    category: "Jacket",
+    price: "40",
+    image: "./src/img/beige-jacket.jpg"
+},
+{
+    productName: "Beige Shirt",
+    category: "Topwear",
+    price: "25",
+    image: "./src/img/beige-shirt.jpg"
+},
+{
+    productName: "Beige Short",
+    category: "Bottomwear",
+    price: "30",
+    image: "./src/img/beige-short.jpg"
+},
+{
+    productName: "Beige Swearter",
+    category: "Topwear",
+    price: "35",
+    image: "./src/img/beige-sweater.jpg"
+},
+{
+    productName: "Black Jogger",
+    category: "Bottomwear",
+    price: "30",
+    image: "./src/img/black-jogger.jpg"
+},
+{
+    productName: "Black T-shirt",
+    category: "Topwear",
+    price: "20",
+    image: "./src/img/black-t-shirt.jpg"
+},
+{
+    productName: "Blue Jean Jacket",
+    category: "Jacket",
+    price: "40",
+    image: "./src/img/blue-jean-jacket.jpg"
+},
+{
+    productName: "Blue Shirt",
+    category: "Topwear",
+    price: "20",
+    image: "./src/img/blue-shirt.jpg"
+},
+{
+    productName: "Brown Watch",
+    category: "Watch",
+    price: "70",
+    image: "./src/img/brown-watch.jpg"
+},
+{
+    productName: "Gray Pants",
+    category: "Bottomwear",
+    price: "40",
+    image: "./src/img/gray-pants.jpg"
+},
+{
+    productName: "Green Jacket",
+    category: "Jacket",
+    price: "50",
+    image: "./src/img/green-jacket.jpg"
+},
+{
+    productName: "Green Pants",
+    category: "Bottomwear",
+    price: "35",
+    image: "./src/img/green-pants.jpg"
+},
+{
+    productName: "Green Shirt",
+    category: "Topwear",
+    price: "20",
+    image: "./src/img/green-shirt.jpg"
+},
+{
+    productName: "Light Blue Pants",
+    category: "Bottomwear",
+    price: "55",
+    image: "./src/img/light-blue-pants.jpg"
+},
+{
+    productName: "Sleeveless Gray Shirt",
+    category: "Topwear",
+    price: "35",
+    image: "./src/img/sleeveless-gray-shirt.jpg"
+},
+{
+    productName: "Sporty Smart Watch",
+    category: "Watch",
+    price: "70",
+    image: "./src/img/sporty-black-watch.jpg"
+},
+]
+
+export default products
